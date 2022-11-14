668b418fc9ac4b133f22470604ebb6db1f74a444
xoctavio <xavieroctavio91@hotmail.com>
2022-11-14 15:09:03 -0600

    Edit index.js
    - Created variables myAge and humanDogRatio
    - Multiplied variables together and stored result in myDogAge
    - Logged myDogAge to the console

--- 1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
+++ 1. Build a Passenger Counter App/5. Basic mathematical operations/index.js
@@ -1,13 +1,18 @@
 // document.getElementById("count").innerText = 5
 
 // cmd+k+c
-// let firstBatch = 5
-// let secondBatch = 7
-// let count = firstBatch + secondBatch
-// console.log(count)
+// let firstBatch = 5;
+// let secondBatch = 7;
+// let count = firstBatch + secondBatch;
+// console.log(count);
 
 
 // 1. Create two variables, myAge and humanDogRatio
-// 2. Multiply the two together and store the result in myDogAge 
-// 3. Log myDogAge to the console
+let myAge = 31;
+let humanDogRatio = 7;
 
+// 2. Multiply the two together and store the result in myDogAge
+let myDogAge = myAge * humanDogRatio;
+
+// 3. Log myDogAge to the console
+console.log(myDogAge);
