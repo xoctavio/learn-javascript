5611d0306a89711fcfa05bb8dcba659cf45f3b60
xoctavio <xavieroctavio91@hotmail.com>
2022-11-14 16:59:46 -0600

    Edit index.js
    - Commented out example code
    - Created bonusPoints variable:
     1. initialized as 50
     2. increased to 100
     3. decreased to 25
     4. increased to 70
    - Console logged each step

--- 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
+++ 1. Build a Passenger Counter App/6. Reassigning and incrementing/index.js
@@ -1,10 +1,19 @@
 // document.getElementById("count").innerText = 5
 
-let count = 5
+// let count = 5
 
-console.log(count)
+// console.log(count)
 
 // Create a variable, bonusPoints. Initialize it as 50. Increase it to 100.
+let bonusPoints = 50;
+console.log(bonusPoints);
+
+bonusPoints += 50;
+console.log(bonusPoints);
+
 // Decrease it down to 25, and then finally increase it to 70
+bonusPoints -= 75;
+console.log(bonusPoints);
 
-
+bonusPoints += 45;
+console.log(bonusPoints);
