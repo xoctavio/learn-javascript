a078ffd738cd6eb9b0f6b7e8d57e4067a34d9374
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 14:52:35 -0600

    Edit index.js
    - Created save function

--- 1. Build a Passenger Counter App/17. Create the save button/index.js
+++ 1. Build a Passenger Counter App/17. Create the save button/index.js
@@ -1,11 +1,13 @@
-let countEl = document.getElementById("count-el")
-let count = 0
+let countEl = document.getElementById("count-el");
+let count = 0;
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count += 1;
+    countEl.innerText = count;
 }
 
 // 1. Create a function, save(), which logs out the count when it's called
-
+function save() {
+    console.log(count);
+}
 
