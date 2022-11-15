8517f8dfe1be74d071a34fc1252301768ffbbcc8
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 10:53:27 -0600

    Edit index.js
    - Created increment function

--- 1. Build a Passenger Counter App/8. The onclick event listener/index.js
+++ 1. Build a Passenger Counter App/8. The onclick event listener/index.js
@@ -4,11 +4,11 @@
 // count = count + 1
 // console.log(count)
 
-// intialize the count as 0
+// initialize the count as 0
 // listen for clicks on the increment button
 // increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
-
-
-
+function increment() {
+ console.log("the button was clicked");
+}
