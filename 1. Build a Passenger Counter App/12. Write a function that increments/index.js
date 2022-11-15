8c01be8aa746a2ddaee240f2a642e1f60455f3c9
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 12:09:40 -0600

    Edit index.js
    - Created addLap function
    - Called addLap three times
    - Added ";" where missing in original code

--- 1. Build a Passenger Counter App/12. Write a function that increments/index.js
+++ 1. Build a Passenger Counter App/12. Write a function that increments/index.js
@@ -1,9 +1,13 @@
-let lapsCompleted = 0
+let lapsCompleted = 0;
 
 // Create a function that increments the lapsCompleted variable with one
 // Run it three times
+function addLap() {
+ lapsCompleted += 1;
+}
 
+addLap();
+addLap();
+addLap();
 
-
-
-console.log(lapsCompleted)
+console.log(lapsCompleted);
