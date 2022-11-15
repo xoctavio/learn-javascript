611a54d0893c2a27fd1e194c1a2c215709ca738b
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 15:13:14 -0600

    Edit index.js
    - Created three variables:
      1. name
      2. greeting
      3. myGreeting
    - Logged myGreeting

--- 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
+++ 1. Build a Passenger Counter App/20. Log a greeting to the console/index.js
@@ -1,7 +1,10 @@
-// Create two variables, name and greeting. The name variable should store your name,
-// and the greeting should store e.g. "Hi, my name is "
+// Create two variables, name and greeting. 
 
-// Create a third variable, myGreeting, that contatenates the two strings
+let name = `xebe`;
+let greeting = `hi, my name is`;
+
+// Create a third variable, myGreeting, that concatenates the two strings
+let myGreeting = `${greeting} ${name}`;
+
 // Log myGreeting to the console
-
-
+console.log(myGreeting);
