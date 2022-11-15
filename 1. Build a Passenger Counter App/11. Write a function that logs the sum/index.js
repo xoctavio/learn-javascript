06d8044535aa3d8f6a8ba5db799423424d132609
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 12:03:38 -0600

    Edit index.js
    - Created and called lapTotal function
    - Created lapTotal variable and added comment about the "let" variable

--- 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
+++ 1. Build a Passenger Counter App/11. Write a function that logs the sum/index.js
@@ -3,4 +3,10 @@ let lap2 = 33
 let lap3 = 36
 
 // Create a function that logs out the sum of all the lap times
+function lapTotal() {
+ // let variable only exists in the scope of the function - it is block-scoped
+ let totalTime = lap1 + lap2 + lap3;
+ console.log(totalTime);
+}
 
+lapTotal();
