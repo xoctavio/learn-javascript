5c732b4a9c0d07627b48aac1e4ec04e09a22479e
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 14:13:19 -0600

    Edit index.js
    - Added ";" where missing in original code

--- 1. Build a Passenger Counter App/15. The Document Object Model/index.js
+++ 1. Build a Passenger Counter App/15. The Document Object Model/index.js
@@ -1,9 +1,7 @@
-let countEl = document.getElementById("count-el")
-let count = 0
+let countEl = document.getElementById("count-el");
+let count = 0;
 
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    count = count + 1;
+    countEl.innerText = count;
 }
-
-
