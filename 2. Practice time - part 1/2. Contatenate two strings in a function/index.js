267d5e16e740320535b2f20ddee3c31e3a7e53aa
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 20:13:41 -0600

    Edit index.js
    - Created and called logGreeting function

--- 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
+++ 2. Practice time - part 1/2. Contatenate two strings in a function/index.js
@@ -2,4 +2,8 @@ let name = "Linda"
 let greeting = "Hi there"
 
 // Create a function that logs out "Hi there, Linda!" when called
+function logGreeting() {
+ console.log(`${greeting}, ${name}! `);
+}
 
+logGreeting();
