fda2011ee0480fafd7f7ae913c0fc9111e8bec9c
xoctavio <xavieroctavio91@hotmail.com>
2022-11-16 16:11:51 -0600

    Edit index.js
    - Created sumEl variable
    - Created add, subtract, divide, and multiply functions

--- 2. Practice time - part 1/6. Calculator challenge/index.js
+++ 2. Practice time - part 1/6. Calculator challenge/index.js
@@ -3,10 +3,30 @@ let num2 = 2
 document.getElementById("num1-el").textContent = num1
 document.getElementById("num2-el").textContent = num2
 
+sumEl = document.getElementById("sum-el");
 // Create four functions: add(), subtract(), divide(), multiply()
 // Call the correct function when the user clicks on one of the buttons
 // Perform the given calculation using num1 and num2
 // Render the result of the calculation in the paragraph with id="sum-el"
+function add() {
+ result = num1 + num2;
+ sumEl.textContent = `Sum: ${result}`;
+}
+
+function subtract() {
+ result = num1 - num2;
+ sumEl.textContent = `Sum: ${result}`;
+}
+
+function divide() {
+ result = num1 / num2;
+ sumEl.textContent = `Sum: ${result}`;
+}
+
+function multiply() {
+ result = num1 * num2;
+ sumEl.textContent = `Sum: ${result}`;
+}
 
 // E.g. if the user clicks on the "Plus" button, you should render
 // "Sum: 10" (since 8 + 2 = 10) inside the paragraph with id="sum-el"
