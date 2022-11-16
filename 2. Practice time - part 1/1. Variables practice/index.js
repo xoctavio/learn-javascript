5ca93562009d2032e7b606b4b855f20636674041
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 20:05:32 -0600

    Edit index.js
    - Created firstName and lastName variables
    - Concatenated into fullName variable
    - Logged fullName to the console

--- 2. Practice time - part 1/1. Variables practice/index.js
+++ 2. Practice time - part 1/1. Variables practice/index.js
@@ -1,7 +1,9 @@
 // Create two variables, firstName and lastName
+let firstName = `xebe`;
+let lastName = `dong`;
 
 // Concatenate the two variables into a third variable called fullName
+let fullName = `${firstName} ${lastName}`;
 
 // Log fullName to the console
-
-
+console.log(fullName);
