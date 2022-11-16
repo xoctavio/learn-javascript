0687d1f80919c5c963ec03aaf7f5a2943fb5de38
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 20:23:33 -0600

    Edit index.js
    - Created add3Points and remove1Point functions
    - Called functions until myPoints = 10

--- 2. Practice time - part 1/3. Incrementing and decrementing/index.js
+++ 2. Practice time - part 1/3. Incrementing and decrementing/index.js
@@ -2,9 +2,19 @@ let myPoints = 3
 
 // Create two functions, add3Points() and remove1Point(), and have them
 // add/remove points to/from the myPoints variable
+function add3points() {
+ myPoints += 3;
+}
 
-
-
+function remove1Point() {
+ myPoints -= 1;
+}
 
 // Call the functions to that the line below logs out 10
+add3points();
+add3points();
+add3points();
+remove1Point();
+remove1Point();
+
 console.log(myPoints)
