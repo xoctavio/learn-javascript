749ab7e457cc2ddd27fe238daaab2b7cdc768448
xoctavio <xavieroctavio91@hotmail.com>
2022-11-15 19:25:34 -0600

    Edit index.js
    - Created saveEl variable
    - Created countDash variable inside save function
    - Added saveEl.innerText inside save function
    - Added ";" where missing in original code

--- 1. Build a Passenger Counter App/25. Create the save feature/index.js
+++ 1. Build a Passenger Counter App/25. Create the save feature/index.js
@@ -1,17 +1,21 @@
 // 1. Grab the save-el paragrah and store it in a variable called saveEl
-let countEl = document.getElementById("count-el")
-let count = 0
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+let saveEl = document.getElementById("save-el");
 
 function increment() {
-    count += 1
-    countEl.innerText = count
+    count += 1;
+    countEl.innerText = count;
 }
 
 function save() {
     // 2. Create a variable that contains both the count and the dash separator, i.e. "12 - "
+    countDash = ` ${count} - `;
     // 3. Render the variable in the saveEl using innerText
+    saveEl.innerText += countDash;
     // NB: Make sure to not delete the existing content of the paragraph
-    console.log(count)
+    console.log(count);
 }
 
 
