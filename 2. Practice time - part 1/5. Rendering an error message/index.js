0bd00b3c68487dd6be82e400639abecaf39440f8
xoctavio <xavieroctavio91@hotmail.com>
2022-11-16 15:31:36 -0600

    Edit index.js
    - Created purchaseError variable
    - Created errorMessage function with purchaseError.textContent variable and error message

--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -1,5 +1,11 @@
 // When the user clicks the purchase button, render out
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
+let purchaseError = document.getElementById("error");
 
+console.log(purchaseError);
+
+function errorMessage() {
+ purchaseError.textContent = "something went wrong, please try again";
+}
 
