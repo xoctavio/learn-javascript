cee5355d79fbfe7f479b7d18f68aa043c2191231
xoctavio <xavieroctavio91@hotmail.com>
2022-11-21 10:13:31 -0600

    Show featuredPosts[] array length

--- 3. Build a Blackjack game/22. Array indexes/index.js
+++ 3. Build a Blackjack game/22. Array indexes/index.js
@@ -6,17 +6,4 @@ let featuredPosts = [
     "I've just relaunched my portfolio"
 ]
 
-
-let experience = [
-    "CEO at Scrimba", 
-    "Frontend developer at Xeneta",
-    "People counter for Norstat"
-]
-
-// Make the following appear in the console:
-// Frontend developer at Xeneta
-// People counter for Norstat
-// CEO at Scrimba
-console.log(experience[1]);
-console.log(experience[2]);
-console.log(experience[0]);
+console.log(featuredPosts.length);
