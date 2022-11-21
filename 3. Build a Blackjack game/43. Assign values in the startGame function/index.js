fda6a87808db839603f3c2e26e4d1c5b8a3f0f10
xoctavio <xavieroctavio91@hotmail.com>
2022-11-21 17:55:59 -0600

    Edit index.js
    - Removed firstCard and secondCard variables
    - Assigned empty array to cards
    - Assigned 0 to sum variable
    - Assigned false to isAlive variable
    - Edited startGame function:
     1. Assigned true to isAlive variable
    	2. Added firstCard and secondCard variables
    	3. Re-assigned the cards array and sum variable

--- 3. Build a Blackjack game/43. Assign values in the startGame function/index.js
+++ 3. Build a Blackjack game/43. Assign values in the startGame function/index.js
@@ -1,9 +1,7 @@
-let firstCard = getRandomCard()
-let secondCard = getRandomCard()
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard
+let cards = [];
+let sum = 0;
 let hasBlackJack = false
-let isAlive = true
+let isAlive = false;
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
@@ -23,8 +21,13 @@ function getRandomCard() {
 }
 
 function startGame() {
+    isAlive = true;
     // Generate two random numbes
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
     // Re-assign the cards and sum variables so that the game can start
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame()
 }
 
