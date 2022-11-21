0c9d22cf4e6b6b078d49dd17a4c401953c605856
xoctavio <xavieroctavio91@hotmail.com>
2022-11-21 10:12:11 -0600

    Log experience[] array items

--- 3. Build a Blackjack game/22. Array indexes/index.js
+++ 3. Build a Blackjack game/22. Array indexes/index.js
@@ -7,9 +7,16 @@ let featuredPosts = [
 ]
 
 
-let experience = ["CEO at Scrimba", "Frontend developer at Xeneta", "People counter for Norstat"]
+let experience = [
+    "CEO at Scrimba", 
+    "Frontend developer at Xeneta",
+    "People counter for Norstat"
+]
 
 // Make the following appear in the console:
 // Frontend developer at Xeneta
 // People counter for Norstat
 // CEO at Scrimba
+console.log(experience[1]);
+console.log(experience[2]);
+console.log(experience[0]);
