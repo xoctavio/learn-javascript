38edf4ad0cd0ea9ed5bb1ae982e1c303055abf74
xoctavio <xavieroctavio91@hotmail.com>
2022-11-21 11:02:26 -0600

    Edit cardsEl.textContent
    - Edited textContent for debugging purposes

--- 3. Build a Blackjack game/25. Creating the cards array/index.js
+++ 3. Build a Blackjack game/25. Creating the cards array/index.js
@@ -20,7 +20,7 @@ function startGame() {
 
 function renderGame() {
     // 2. Refer to the cards array when rendering out the cards
-    cardsEl.textContent += ` ${cards[0]} ${cards[1]}`;
+    cardsEl.textContent = "Cards: " + cards[0] + " " + cards[1];
     sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card?"
