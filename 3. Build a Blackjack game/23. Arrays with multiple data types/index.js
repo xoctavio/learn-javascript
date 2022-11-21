cc2d265b0b05c0a0ab3f9413526663da408beb1a
xoctavio <xavieroctavio91@hotmail.com>
2022-11-21 10:30:02 -0600

    Create whoAmI array

--- 3. Build a Blackjack game/23. Arrays with multiple data types/index.js
+++ 3. Build a Blackjack game/23. Arrays with multiple data types/index.js
@@ -2,3 +2,9 @@
 
 // Create an array that describes yourself. Use the three primitive data types you've learned
 // It should contain your name (string), your age (number), and whether you like pizza (boolean)
+
+let whoAmI = [
+ "xebe",
+ 10000,
+ true
+];
