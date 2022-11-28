8888294bf1f85791ff62a2ab45655619a7663e4e
xoctavio <xavieroctavio91@hotmail.com>
2022-11-27 21:57:05 -0600

    Create player object
    - Created  player object with two keys, name and chips

--- 3. Build a Blackjack game/52. Use an object to store player data/index.js
+++ 3. Build a Blackjack game/52. Use an object to store player data/index.js
@@ -1,4 +1,8 @@
 // 2. Create the player object. Give it two keys, name and chips, and set their values
+let player = {
+    name: "xeble",
+    chips: 145
+};
 let cards = []
 let sum = 0
 let hasBlackJack = false
