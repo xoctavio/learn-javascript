818d243372038507fe50a6afbae4f0a2fde178d5
xoctavio <xavieroctavio91@hotmail.com>
2022-11-27 21:53:43 -0600

    Create object
    - Created castle object
    - Logged out two keys using the dot notation

--- 3. Build a Blackjack game/51. Create your first object/index.js
+++ 3. Build a Blackjack game/51. Create your first object/index.js
@@ -2,4 +2,12 @@
 // It should contain at least one boolean, one string, one number, and one array
 // Log out at least two of the keys using the dot notation
 
+let castle = {
+ isOld: true,
+ name: "casterly rock",
+ age: 430,
+ amenities: ["moat", "dragons"]
+};
 
+console.log(castle.isOld);
+console.log(castle.name);
