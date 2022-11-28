84f83b2c2d91ffcc72ca74c8cc37ef30166300f1
xoctavio <xavieroctavio91@hotmail.com>
2022-11-27 22:04:09 -0600

    Render player's name and chips in playerEl

--- 3. Build a Blackjack game/52. Use an object to store player data/index.js
+++ 3. Build a Blackjack game/52. Use an object to store player data/index.js
@@ -3,6 +3,7 @@ let player = {
     name: "xeble",
     chips: 145
 };
+
 let cards = []
 let sum = 0
 let hasBlackJack = false
@@ -13,7 +14,9 @@ let sumEl = document.getElementById("sum-el")
 let cardsEl = document.getElementById("cards-el")
 // 3. Grab ahold of the player-el paragraph and store it in a variable called playerEl
 let playerEl = document.querySelector("#player-el");
+
 // 4. Render the player's name and chips in playerEl
+playerEl.textContent = `${player.name}: $${player.chips}`;
 
 function getRandomCard() {
     let randomNumber = Math.floor( Math.random()*13 ) + 1
