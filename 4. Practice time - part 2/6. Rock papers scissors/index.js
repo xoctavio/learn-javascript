7916951cb618c6fe46fdd1dfda365cdf27d68bba
xoctavio <xavieroctavio91@hotmail.com>
2022-12-01 12:22:58 -0600

    Create function
    - Created pick() function that returns a random item from the array

--- 4. Practice time - part 2/6. Rock papers scissors/index.js
+++ 4. Practice time - part 2/6. Rock papers scissors/index.js
@@ -1,5 +1,9 @@
 let hands = ["rock", "paper", "scissor"]
 
 // Create a function that returns a random item from the array
+function pick() {
+ let random = Math.floor(Math.random() * 3);
+ return hands[random];
+}
 
-
+console.log(pick());
