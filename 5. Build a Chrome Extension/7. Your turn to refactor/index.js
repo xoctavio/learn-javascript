e99362416f8d0b6e337ed794d84854535937dcb5
xoctavio <xavieroctavio91@hotmail.com>
2022-12-05 16:55:02 -0600

    Refactor function
    - Created variable
    - Deleted function and added event listener to variable

--- 5. Build a Chrome Extension/7. Your turn to refactor/index.js
+++ 5. Build a Chrome Extension/7. Your turn to refactor/index.js
@@ -1,9 +1,10 @@
 // Refactor the code so that it uses .addEventListener()
 // when you click the SAVE INPUT button
 
-function saveLead() {
-    console.log("Button clicked!")
-}
+let inputBtn = document.querySelector("#input-btn");
 
+inputBtn.addEventListener("click", function() {
+    console.log("Button clicked!")
+})
 
 
