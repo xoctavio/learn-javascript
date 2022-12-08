a028945eff589e23ce72ae13b1dadb1f560504e6
xoctavio <xavieroctavio91@hotmail.com>
2022-12-08 15:09:51 -0600

    Render a paragraph under the button
    - Added onclick attribute to button
    - Created buy() function that renders a paragraph under the button when clicked

--- 5. Build a Chrome Extension/17.  More innerHTML practice/index.js
+++ 5. Build a Chrome Extension/17.  More innerHTML practice/index.js
@@ -1,7 +1,10 @@
 
 const container = document.getElementById("container")
 
-container.innerHTML = "<button>Buy!</button>"
+container.innerHTML = `<button onclick= "buy();">Buy!</button>`;
 
 // When clicked, render a paragraph under the button (in the container)
 // that says "Thank you for buying!"
+function buy() {
+ container.innerHTML += `<p>Thank you for buying!</p>`;
+}
