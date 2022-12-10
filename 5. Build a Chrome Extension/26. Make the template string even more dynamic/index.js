8b2ee5ab488e85e64509864e0a81eb719da4c0f5
xoctavio <xavieroctavio91@hotmail.com>
2022-12-10 09:45:18 -0600

    Add variable to template string
    - Created sender variable, and set its value to a name
    - Used sender variable instead of "Per" in template string

--- 5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
+++ 5. Build a Chrome Extension/26. Make the template string even more dynamic/index.js
@@ -2,8 +2,8 @@
 
 const recipient = "James"
 // Create a new variable, sender, and set its value to your name
-
+const sender = "Per"
 // Use your sender variable instead of "Per"
-const email = `Hey ${recipient}! How is it going? Cheers Per`
+const email = `Hey ${recipient}! How is it going? Cheers ${sender}`
 
 console.log(email)
