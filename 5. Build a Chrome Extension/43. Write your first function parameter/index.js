9c7e58c9dc966fb7a4180e18daeb213f2688574d
xoctavio <xavieroctavio91@hotmail.com>
2022-12-12 12:03:40 -0600

    Create function parameter (greeting)

--- 5. Build a Chrome Extension/43. Write your first function parameter/index.js
+++ 5. Build a Chrome Extension/43. Write your first function parameter/index.js
@@ -1,8 +1,8 @@
 const welcomeEl = document.getElementById("welcome-el")
 
 // Give the function a parameter, greeting, that replaces "Welcome back"
-function greetUser() {
-    welcomeEl.textContent = "Welcome back, Per Harald Borgen 👋"    
+function greetUser(greeting) {
+    welcomeEl.textContent = `${greeting}, Per Harald Borgen 👋`;    
 }
 
-greetUser()
+greetUser("Howdy");
