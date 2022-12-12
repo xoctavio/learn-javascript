9b2ae50481662ef53ebca26950682e7ccd5392d5
xoctavio <xavieroctavio91@hotmail.com>
2022-12-12 16:30:50 -0600

    Create function
    - Created getFirst(arr) function
    - Created myArray[]
    - Called function with myArray as an argument

--- 5. Build a Chrome Extension/47. Arrays as parameters/index.js
+++ 5. Build a Chrome Extension/47. Arrays as parameters/index.js
@@ -1,5 +1,9 @@
 // Create a function, getFirst(arr), that returns the first item in the array
+function getFirst(arr) {
+ return arr[0];
+}
 
-
+let myArray = ["pokemon", "digimon"];
 
 // Call it with an array as an argument to verify that it works
+console.log(getFirst(myArray));
