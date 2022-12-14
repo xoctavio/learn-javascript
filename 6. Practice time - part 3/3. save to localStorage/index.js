0f68e9387eddc8b2b205c534799d2b3e4a46c6a6
xoctavio <xavieroctavio91@hotmail.com>
2022-12-13 18:27:06 -0600

    Save to localStorage
    - Saved value to localStorage
    - Deleted code and refreshed the page
    - Fetched value from localStorage and logged it out

--- 6. Practice time - part 3/3. save to localStorage/index.js
+++ 6. Practice time - part 3/3. save to localStorage/index.js
@@ -1,3 +1,6 @@
 // Save a value to localStorage
 // Delete your code and refresh the page
 // Fetch your value from localStorage and log it out
+let pokemon = localStorage.getItem("pokemon");
+
+console.log(pokemon);
