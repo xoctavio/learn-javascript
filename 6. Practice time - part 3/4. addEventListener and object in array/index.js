ba8deb795a725f1c1595c0c8a1a222e80858aac5
xoctavio <xavieroctavio91@hotmail.com>
2022-12-14 13:50:43 -0600

    Edit index.js
    - Fetched button from the DOM
    - Stored button in scoreBtn variable
    - Used addEventListener() to listen for button clicks
    - Logged Jane's score when the button is clicked

--- 6. Practice time - part 3/4. addEventListener and object in array/index.js
+++ 6. Practice time - part 3/4. addEventListener and object in array/index.js
@@ -12,3 +12,8 @@ let data = [
 // Fetch the button from the DOM, store it in a variable
 // Use addEventListener() to listen for button clicks
 // Log Jane's score when the button is clicked (via data)
+let scoreBtn = document.querySelector("#score-btn");
+
+scoreBtn.addEventListener("click", function() {
+    console.log(data[0].score);
+})
